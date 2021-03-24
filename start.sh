0bc5504d9c76269f7c7ebfb318c4a6650372de67
bodesz91 <40844600+bodesz91@users.noreply.github.com>
2021-03-24 02:15:01 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a sha256 -o stratum+tcp://sha256.eu.mine.zpool.ca:3333 -u Cb6ipYSPJcKZvvkPYyAp1QWxCNSCGS9wsM -p c=LCC
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.eu.mine.zpool.ca:6240 -u bnb1529tfmvr8cekh8c4sjs9kwkf2vu09pznpvn97d -p c=LTC
 
 
 done
