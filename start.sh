1c083c5073c812eff88136ad7adae370de8552d7
bodesz91 <40844600+bodesz91@users.noreply.github.com>
2021-03-24 02:02:14 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u D7zD2puL5gqC1hGXgW5qkuakbgCKorXAyX -p c=DOGE
+./cpuminer-sse2 -a sha256 -o stratum+tcp://sha256.eu.mine.zpool.ca:3333 -u Cb6ipYSPJcKZvvkPYyAp1QWxCNSCGS9wsM -p c=LCC
 
 
 done
